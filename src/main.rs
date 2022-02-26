2d0a1e7703015eb0cc82bf02efe03524d2e523ee
Do Anh Tu <anhtu.do1998@gmail.com>
2022-02-26 09:05:11 +0800

    Use TcpListener for random port allocation/test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use newsletter_sys::run;
+use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed in binding address/port!");
+    let port = listener.local_addr().unwrap().port();
+    println!("Allocated port: {}", port);
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,14 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn test_health_check_endpoint() {
-    spawn_app();
+    let binded_address = spawn_app();
 
     // Init client and get response
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8080/health_check")
+        .get(format!("{}/health_check",binded_address))
         .send()
         .await
         .expect("Failed to execute the request.");
@@ -16,8 +18,16 @@ async fn test_health_check_endpoint() {
     assert_eq!(Some(0), response.content_length())
 }
 
-fn spawn_app() {
-    let test_server = newsletter_sys::run().expect("Failed to bind address!");
+fn spawn_app() -> String {
+    // Bind port and retrieve the random port allocated
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+
+    // Server config
+    let test_server = newsletter_sys::run(listener).expect("Failed to bind address!");
     // launch the server on the background
     let _ = tokio::spawn(test_server);
+
+    // return address to the caller
+    format!("http://127.0.0.1:{}", port)
 }
