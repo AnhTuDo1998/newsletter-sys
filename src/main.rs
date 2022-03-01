4d96639674cb8a2654955d3f886b95f14eadfb5f
Do Anh Tu <anhtu.do1998@gmail.com>
2022-03-01 23:06:51 +0800

    Added logging for subscriptions POST (manual) and actix functions (lib)

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use newsletter_sys::configuration::get_configuration;
 use newsletter_sys::startup::run;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use env_logger::Env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     // Read configuration
     let configs = get_configuration().expect("Failed to read configs!");
     // DB connection
