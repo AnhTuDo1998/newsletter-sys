02b54f73f0b50e92677a388bbba95ef4d5de71e3
Do Anh Tu <anhtu.do1998@gmail.com>
2022-04-21 11:55:38 +0800

    Forward Log events to tracer

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,14 @@ use std::net::TcpListener;
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_log::LogTracer;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Default will be at INFO level
+    // Forward Log event to Trace
+    LogTracer::init().expect("Failed to set logger");
+
+    // Default Trace will be at INFO level
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
     let formatting_layer = BunyanFormattingLayer::new("newsletter-sys".into(), std::io::stdout);
     let subscriber = Registry::default().with(env_filter).with(JsonStorageLayer).with(formatting_layer);
